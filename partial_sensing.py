9b71753e5c09ed044d2dfea25e63bfaed0d603e7
ddrkaser <krisleecn@outlook.com>
2021-10-10 00:43:32 -0400

    Update agent3. Now agent3 should be working as intended, the efficiency(cells be processed) should be agent2 > agent3 > agent1.

--- partial_sensing.py
+++ partial_sensing.py
@@ -38,7 +38,7 @@ class Cell:
     def getPos(self):
         return self.col, self.row
     #return a list of neighbors of the current cell
-    def findneighbors(self,grid):
+    def findneighbors(self):
         dim = self.dim
         y, x = self.getPos()
         neighbors = [(y2, x2) for x2 in range(x-1, x+2)                     
@@ -49,48 +49,50 @@ class Cell:
                                   (0 <= x2 < dim) and
                                   (0 <= y2 < dim))]
         return neighbors
-    #unfinished
-    #now can only update kb.c and kb.n
+    #sense the neigbhors, update current cell's info
     def sensing(self, knowledge):
-        neighbors = knowledge[self.row][self.col].findneighbors(grid)
-        self.blocked = 0
-        self.visited = True
+        neighbors = knowledge[self.row][self.col].findneighbors()
+        #self.blocked = 0
+        #self.visited = True
         c = 0
         b = 0
         e = 0
         h = 0
         for cell in neighbors:
-            y, x = cell
-            if grid[x][y] == 1:
+            x, y = cell
+            if grid[y][x] == 1:
                 c += 1
-            if knowledge[x][y].blocked == 1:
+            if knowledge[y][x].blocked == 1:
                 b += 1
-            if knowledge[x][y].blocked == 0:
-                e += 0
-            if knowledge[x][y].blocked == 9999:
+            if knowledge[y][x].blocked == 0:
+                e += 1
+            if knowledge[y][x].blocked == 9999:
                 h += 1
         self.c = c
         self.b = b
         self.e = e
         self.h = h
         self.n = len(neighbors)
+        """
         if self.c == self.b:
             self.h == 0
             for cell in neighbors:
-                y, x = cell
-                if knowledge[x][y].blocked == 9999:
-                    knowledge[x][y].blocked = 0
+                x, y = cell
+                if knowledge[y][x].blocked == 9999:
+                    knowledge[y][x].blocked = 0
         if self.n - self.c == self.e:
             self.h == 0
             for cell in neighbors:
-                y, x = cell
-                if knowledge[x][y].blocked == 9999:
-                    knowledge[x][y].blocked = 1
-                                
+                x, y = cell
+                if knowledge[y][x].blocked == 9999:
+                    knowledge[y][x].blocked = 1
+        """
+                         
     def __lt__(self, other):
         return False
 
 #generate kb based on the grid
+#unblocked = True, the knowledge treats grid as unblocked
 def generate_knowledge(grid):
     cell_list = []
     rows = len(grid)
@@ -102,6 +104,41 @@ def generate_knowledge(grid):
             #cellOBJ.blocked = grid[i][j]
             cell_list[i].append(cellOBJ)
     return cell_list
+
+def infering(y,x,knowledge):
+    queue = [(x,y)]
+    queue = check_queue(y,x,knowledge,queue)
+    while len(queue) != 0:
+        for item in queue:
+            queue.remove(item)
+            x1, y1 = item
+            neighbors = knowledge[y1][x1].findneighbors()
+            knowledge[y1][x1].sensing(knowledge)
+            if knowledge[y1][x1].c == knowledge[y1][x1].b:
+                knowledge[y1][x1].h == 0
+                for neighbor in neighbors:
+                    x2, y2 = neighbor
+                    if knowledge[y2][x2].blocked == 9999:
+                        knowledge[y2][x2].blocked = 0
+                        #if a neighbor was updated, then add its neigbhors to queue
+                        queue = check_queue(y2,x2,knowledge,queue)
+            if knowledge[y1][x1].n - knowledge[y1][x1].c == knowledge[y1][x1].e:
+                knowledge[y1][x1].h == 0
+                for neighbor in neighbors:
+                    x2, y2 = neighbor
+                    if knowledge[y2][x2].blocked == 9999:
+                        knowledge[y2][x2].blocked = 1
+                        #if a neighbor was updated, then add its neigbhors to queue
+                        queue = check_queue(y2,x2,knowledge,queue)
+    return knowledge
+
+#Add (x,y) neighbors which has been visted and h != 0
+def check_queue(y,x,knowledge,queue):
+    for neigbor in knowledge[y][x].findneighbors():
+        x2, y2 = neigbor
+        if knowledge[y2][x2].h != 0 and knowledge[y2][x2].visited == True and neigbor not in queue:
+            queue.append(neigbor)
+    return queue
 
 def A_star(curr_knowledge, start, end):
     # Initializes the g(x), f(x), and h(x) values for all squares
@@ -136,20 +173,33 @@ def A_star(curr_knowledge, start, end):
                 path_pointer = parent[path_pointer]
             shortest_path.append(start)
             shortest_path = shortest_path[::-1]
-            return [shortest_path, cell_count]
-			
+            return [shortest_path, cell_count]	
 		# Determine which neighbors are valid successors
+        #add unblocked cells first, otherwise add unknown cells
         if curr_pos[0] > 0 and curr_knowledge[curr_pos[1]][curr_pos[0] - 1].blocked == 0: # the current node has a neighbor to its left which is unblocked
+            left_neighbor = (curr_pos[0] - 1, curr_pos[1])
+            if g[left_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
+                successors.append(left_neighbor)
+        elif curr_pos[0] > 0 and curr_knowledge[curr_pos[1]][curr_pos[0] - 1].blocked == 9999:
             left_neighbor = (curr_pos[0] - 1, curr_pos[1])
             if g[left_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
                 successors.append(left_neighbor)
 				
         if curr_pos[0] < len(curr_knowledge[0])  - 1 and curr_knowledge[curr_pos[1]][curr_pos[0] + 1].blocked == 0: # the current node has a neighbor to its right which is unblocked
             right_neighbor = (curr_pos[0] + 1, curr_pos[1])
+            if g[right_neighbor] > g[curr_pos] + 1:
+                #if neighbor is undiscovered
+                successors.append(right_neighbor)
+        elif curr_pos[0] < len(curr_knowledge[0])  - 1 and curr_knowledge[curr_pos[1]][curr_pos[0] + 1].blocked == 9999: # the current node has a neighbor to its right which is unblocked
+            right_neighbor = (curr_pos[0] + 1, curr_pos[1])
             if g[right_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
                 successors.append(right_neighbor)
-		
+        
         if curr_pos[1] > 0 and curr_knowledge[curr_pos[1] - 1][curr_pos[0]].blocked == 0: # the current node has a neighbor to its top which is unblocked
+            top_neighbor = (curr_pos[0], curr_pos[1] - 1)
+            if g[top_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
+                successors.append(top_neighbor)
+        elif curr_pos[1] > 0 and curr_knowledge[curr_pos[1] - 1][curr_pos[0]].blocked == 9999: # the current node has a neighbor to its top which is unblocked
             top_neighbor = (curr_pos[0], curr_pos[1] - 1)
             if g[top_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
                 successors.append(top_neighbor)
@@ -158,7 +208,10 @@ def A_star(curr_knowledge, start, end):
             bottom_neighbor = (curr_pos[0], curr_pos[1] + 1)
             if g[bottom_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
                 successors.append(bottom_neighbor)
-				
+        elif curr_pos[1] < len(curr_knowledge) - 1 and curr_knowledge[curr_pos[1] + 1][curr_pos[0]].blocked == 9999: # the current node has a neighbor to its bottom which is unblocked
+            bottom_neighbor = (curr_pos[0], curr_pos[1] + 1)
+            if g[bottom_neighbor] > g[curr_pos] + 1: # if neighbor is undiscovered
+                successors.append(bottom_neighbor)				
         # Update shortest paths and parents for each valid successor and add to priority queue, per assignment instructions
         for successor in successors:
             g[successor] = g[curr_pos] + 1
@@ -172,17 +225,12 @@ def A_star(curr_knowledge, start, end):
             return False
 
 # Handles processing of Repeated A*, restarting that algorithm if a blocked square is found in the determined shortest path
-def algorithmA(grid, start, end, is_grid_known, has_four_way_vision):
+def algorithmA(grid, start, end, has_four_way_vision):
     # The assumed state of the gridworld at any point in time. For some questions, the current knowledge is unknown at the start
-    curr_knowledge = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-    final_discovered = [[1 for i in range(len(grid))] for j in range(len(grid[0]))]
-    final_discovered[0][0] = 0
-    final_discovered[-1][-1] = 0
+    curr_knowledge = generate_knowledge(grid)
     # If the grid is considered known to the robot, operate on that known grid
 	# Else, the robot assumes a completely unblocked gridworld and will have to discover it as it moves
-    if is_grid_known:
-        curr_knowledge = grid
-    complete_path = []
+    complete_path = [(0,0)]
 	# Run A* once on grid as known, returning False if unsolvable
     shortest_path = A_star(curr_knowledge, start, end)
     if not shortest_path:
@@ -194,14 +242,11 @@ def algorithmA(grid, start, end, is_grid_known, has_four_way_vision):
         for sq in shortest_path[0]:
             x = sq[0]
             y = sq[1]
-            final_discovered[y][x] = 0
 			# If blocked, rerun A* and restart loop
             if grid[y][x] == 1:
                 # If the robot can only see squares in its direction of movement, update its current knowledge of the grid to include this blocked square
                 if not has_four_way_vision:
-                    curr_knowledge[y][x] = 1
-                    final_discovered[y][x] = 1
-                complete_path.remove(prev_sq)
+                    curr_knowledge[y][x].blocked = 1
                 shortest_path = A_star(curr_knowledge, prev_sq, end)                
                 if not shortest_path:
                     return False
@@ -210,40 +255,85 @@ def algorithmA(grid, start, end, is_grid_known, has_four_way_vision):
                 break
 			# If new square unblocked, update curr_knowledge. Loop will restart and move to next square on presumed shortest path
             else:
-                complete_path.append(sq)
+                if sq != complete_path[-1]:
+                    complete_path.append(sq)
                 # If the robot can see in all compass directions, update squares adjacent to its current position
                 if has_four_way_vision:
                      if x != 0:
-                         curr_knowledge[y][x - 1] = grid[y][x - 1]
-                         final_discovered[y][x - 1] = grid[y][x - 1]
+                         curr_knowledge[y][x - 1].blocked = grid[y][x - 1]
                      if x < len(curr_knowledge[0]) - 1:
-                         curr_knowledge[y][x + 1] = grid[y][x + 1]
-                         final_discovered[y][x + 1] = grid[y][x + 1]
+                         curr_knowledge[y][x + 1].blocked = grid[y][x + 1]
                      if y != 0:
-                         curr_knowledge[y - 1][x] = grid[y - 1][x]
-                         final_discovered[y - 1][x] = grid[y - 1][x]
+                         curr_knowledge[y - 1][x].blocked = grid[y - 1][x]
                      if y < len(curr_knowledge) - 1:
-                         curr_knowledge[y + 1][x] = grid[y + 1][x]
-                         final_discovered[y + 1][x] = grid[y + 1][x]
+                         curr_knowledge[y + 1][x].blocked = grid[y + 1][x]
             prev_sq = sq
         if not is_broken:
             break
         is_broken = False
-    return [complete_path, cell_count, final_discovered]
+    return [complete_path, cell_count]
 
+def inference(grid, start, end):
+    #generate initial shortest path, assumeing the grid is unblocked
+    shortest_path = A_star(generate_knowledge(grid), start, end)
+    curr_knowledge = generate_knowledge(grid)
+    complete_path = [(0,0)]
+    if not shortest_path:
+        return False
+    is_broken = False
+    cell_count = shortest_path[1]
+    while True:
+		# Move pointer square by square along path
+        for sq in shortest_path[0]:
+            x = sq[0]
+            y = sq[1]
+			# If blocked, rerun A* and restart loop            
+            if grid[y][x] == 1:
+                curr_knowledge[y][x].blocked = 1
+                curr_knowledge[prev_sq[1]][prev_sq[0]].sensing(curr_knowledge)
+                curr_knowledge = infering(prev_sq[1],prev_sq[0],curr_knowledge)
+                shortest_path = A_star(curr_knowledge, prev_sq, end)
+                #print(shortest_path)
+                if not shortest_path:
+                    return False
+                is_broken = True
+                cell_count += shortest_path[1]
+                break
+			# If new square unblocked, update curr_knowledge. Loop will restart and move to next square on presumed shortest path
+            else:
+                if sq != complete_path[-1]:
+                    complete_path.append(sq)
+                curr_knowledge[y][x].blocked = 0
+                curr_knowledge[y][x].visited = True
+                if curr_knowledge[y][x].h != 0:
+                    curr_knowledge[y][x].sensing(curr_knowledge)
+                    curr_knowledge = infering(y,x,curr_knowledge)
+                    shortest_path = A_star(curr_knowledge, sq, end)                 
+            prev_sq = sq
+        if not is_broken:
+            break
+        is_broken = False
+    return [complete_path, cell_count]    
+    
 #test
 #PLEASE BE NOTED use knowledge[row][col] to retraive a cell!!!!
-grid = generate_gridworld(10,9,.3)
+grid = generate_gridworld(15,15,.3)
 knowledge = generate_knowledge(grid)
 #should return (1,0)
 knowledge[0][1].getPos()
 #should return 9999
 knowledge[0][1].blocked
 #should return list of neighbors
-knowledge[0][1].findneighbors(grid)
+knowledge[0][1].findneighbors()
 #should return correct c
 knowledge[1][2].sensing(knowledge)
 knowledge[1][2].c
 knowledge[1][2].n
 knowledge[1][2].b
 knowledge[1][2].h
+#test searching algo
+start = (0,0)
+end = (14,14)
+agent1 = algorithmA(grid, start, end, has_four_way_vision = False)
+agent2 = algorithmA(grid, start, end, has_four_way_vision = True)
+agent3 = inference(grid, start, end)
