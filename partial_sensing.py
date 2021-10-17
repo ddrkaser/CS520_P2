a55e70de4180cfa6d9d8e09523880395cd91bbb3
ddrkaser <krisleecn@outlook.com>
2021-10-16 22:13:23 -0400

    add agent4

--- partial_sensing.py
+++ partial_sensing.py
@@ -5,6 +5,7 @@ import pandas as pd
 from math import sqrt
 import matplotlib.pyplot as plt 
 import time
+import copy
 
 # Generates a length * width size gridworld
 # Each square has a probability chance of being blocked 0 means unblocked, 1 means blocked
@@ -88,16 +89,29 @@ def generate_knowledge(grid):
             cell_list[i].append(cellOBJ)
     return cell_list
 
+
+#Add (x,y) neighbors which has been visited and h != 0
+def add_visited_neighbors(y,x,knowledge,queue):
+    for neigbor in knowledge[y][x].findneighbors():
+        x2, y2 = neigbor
+        if knowledge[y2][x2].h != 0 and knowledge[y2][x2].visited == True and neigbor not in queue:
+            queue.append(neigbor)
+    return queue
+
 #inferencing info of current cell, if updated then propagate to its visited neighbors and so on
 def infering(y,x,knowledge):
     queue = [(x,y)]
-    queue = check_queue(y,x,knowledge,queue)
+    queue = add_visited_neighbors(y,x,knowledge,queue)
     while len(queue) != 0:
         for item in queue:
             queue.remove(item)
             x1, y1 = item
             neighbors = knowledge[y1][x1].findneighbors()
             knowledge[y1][x1].sensing(knowledge)
+            if knowledge[y1][x1].n < (knowledge[y1][x1].c + knowledge[y1][x1].e):
+                return False
+            elif knowledge[y1][x1].b > knowledge[y1][x1].c:
+                return False
             if knowledge[y1][x1].c == knowledge[y1][x1].b:
                 knowledge[y1][x1].h == 0
                 for neighbor in neighbors:
@@ -105,25 +119,85 @@ def infering(y,x,knowledge):
                     if knowledge[y2][x2].blocked == 9999:
                         knowledge[y2][x2].blocked = 0
                         #if a neighbor was updated, then add its visited neigbhors to queue
-                        queue = check_queue(y2,x2,knowledge,queue)
-            if knowledge[y1][x1].n - knowledge[y1][x1].c == knowledge[y1][x1].e:
+                        queue = add_visited_neighbors(y2,x2,knowledge,queue)
+            elif knowledge[y1][x1].n - knowledge[y1][x1].c == knowledge[y1][x1].e:
                 knowledge[y1][x1].h == 0
                 for neighbor in neighbors:
                     x2, y2 = neighbor
                     if knowledge[y2][x2].blocked == 9999:
                         knowledge[y2][x2].blocked = 1
                         #if a neighbor was updated, then add its visited neigbhors to queue
-                        queue = check_queue(y2,x2,knowledge,queue)
+                        queue = add_visited_neighbors(y2,x2,knowledge,queue)
+    #if knowledge[y1][x1].c == 1 and knowledge[y1][x1].h != 0:
     return knowledge
 
-#Add (x,y) neighbors which has been visted and h != 0
-def check_queue(y,x,knowledge,queue):
-    for neigbor in knowledge[y][x].findneighbors():
-        x2, y2 = neigbor
-        if knowledge[y2][x2].h != 0 and knowledge[y2][x2].visited == True and neigbor not in queue:
-            queue.append(neigbor)
+def add_hidden_neighbors(y,x,knowledge):
+    queue = []
+    for neighbor in knowledge[y][x].findneighbors():
+        x2, y2 = neighbor
+        if knowledge[y2][x2].blocked == 9999 and neighbor not in queue:
+            queue.append(neighbor)
     return queue
 
+#add visited neigbhors of (x,y) two cells away
+def add_far_neighbors(y,x,knowledge):
+    dim = len(knowledge)
+    neighbors = knowledge[y][x].findneighbors()
+    far_neighbors = [(y2, x2) for x2 in range(x-2, x+3)                     
+                              for y2 in range(y-2, y+3)
+                              if (-1 < x < dim and
+                                  -1 < y < dim and
+                                  (x != x2 or y != y2) and
+                                  (0 <= x2 < dim) and
+                                  (0 <= y2 < dim) and (y2,x2) not in neighbors)]
+    return far_neighbors
+
+#expert system to assume an undiscovered neighbor's block/unblock see if it contradicts current knowledge    
+def expert(y1,x1,knowledge,need_update):
+    hidden_neighbors = add_hidden_neighbors(y1,x1,knowledge)
+    for hidden_neighbor in hidden_neighbors:
+            x2, y2 = hidden_neighbor
+            if knowledge[y1][x1].c - knowledge[y1][x1].b == 1:
+                knowledge_trail = copy.deepcopy(knowledge)
+                knowledge_trail[y2][x2].blocked = 1
+                knowledge_trail = infering(y1,x1,knowledge_trail)
+                if knowledge_trail == False:
+                    del knowledge_trail
+                    knowledge[y2][x2].blocked = 0
+                    knowledge = infering(y1,x1,knowledge)
+                    far_neighbors = add_far_neighbors(y1,x1,knowledge)
+                    for cell in far_neighbors:
+                        x3, y3 = cell
+                        if (knowledge[y3][x3].c - knowledge[y3][x3].b == 1 or knowledge[y3][x3].n - knowledge[y3][x3].c - knowledge[y3][x3].e == 1) and knowledge[y3][x3].h != 0 and cell not in need_update:
+                            need_update.append(cell)
+                    break
+            if knowledge[y1][x1].n - knowledge[y1][x1].c - knowledge[y1][x1].e == 1:
+                knowledge_trail = copy.deepcopy(knowledge)
+                knowledge_trail[y2][x2].blocked = 0
+                knowledge_trail = infering(y1,x1,knowledge_trail)
+                if knowledge_trail == False:
+                    del knowledge_trail
+                    knowledge[y2][x2].blocked = 1
+                    knowledge = infering(y1,x1,knowledge)
+                    far_neighbors = add_far_neighbors(y1,x1,knowledge)
+                    for cell in far_neighbors:
+                        x3, y3 = cell
+                        if (knowledge[y3][x3].c - knowledge[y3][x3].b == 1 or knowledge[y3][x3].n - knowledge[y3][x3].c - knowledge[y3][x3].e == 1) and knowledge[y3][x3].h != 0 and cell not in need_update:
+                            need_update.append(cell)
+                    break
+    print(need_update)
+    return knowledge, need_update
+
+#run expertsystem, till no info to update
+def run_expert(y,x,knowledge):
+    need_update = [(x,y)]
+    while len(need_update) != 0:
+        for item in need_update:
+            x1,y1 = item
+            need_update.remove(item)
+            knowledge, need_update = expert(y1,x1,knowledge,need_update)
+    return knowledge
+            
 def A_star(curr_knowledge, start, end):
     # Initializes the g(x), f(x), and h(x) values for all squares
     g = {(x, y):float("inf") for y, eachRow in enumerate(curr_knowledge) for x, eachcolumn in enumerate(eachRow)}
@@ -242,7 +316,7 @@ def algorithmA(grid, start, end, has_four_way_vision):
         is_broken = False
     return [complete_path, cell_count]
 
-def inference(grid, start, end):
+def inference(grid, start, end, is_expert = False):
     #generate initial shortest path
     shortest_path = A_star(generate_knowledge(grid), start, end)
     curr_knowledge = generate_knowledge(grid)
@@ -259,7 +333,11 @@ def inference(grid, start, end):
 			# If blocked, rerun A* and restart loop            
             if grid[y][x] == 1:
                 curr_knowledge[y][x].blocked = 1
-                curr_knowledge = infering(prev_sq[1],prev_sq[0],curr_knowledge)
+                x1, y1 = prev_sq
+                curr_knowledge = infering(y1,x1,curr_knowledge)
+                if is_expert == True:
+                    if (curr_knowledge[y1][x1].c - curr_knowledge[y1][x1].b == 1 or curr_knowledge[y1][x1].n - curr_knowledge[y1][x1].c - curr_knowledge[y1][x1].e == 1) and curr_knowledge[y1][x1].h != 0:
+                        curr_knowledge = run_expert(y1,x1,curr_knowledge)
                 shortest_path = A_star(curr_knowledge, prev_sq, end)
                 #print(shortest_path)
                 if not shortest_path:
@@ -275,7 +353,11 @@ def inference(grid, start, end):
                 curr_knowledge[y][x].visited = True
                 if curr_knowledge[y][x].h != 0:
                     curr_knowledge = infering(y,x,curr_knowledge)
-                    shortest_path = A_star(curr_knowledge, sq, end)                 
+                    if is_expert == True:
+                        if (curr_knowledge[y][x].c - curr_knowledge[y][x].b == 1) or (curr_knowledge[y][x].n - curr_knowledge[y][x].c - curr_knowledge[y][x].e == 1):
+                            curr_knowledge = run_expert(y,x,curr_knowledge)
+                    shortest_path = A_star(curr_knowledge, sq, end)
+                    print(sq)
             prev_sq = sq
         if not is_broken:
             break
@@ -299,9 +381,12 @@ knowledge[1][2].n
 knowledge[1][2].b
 knowledge[1][2].h
 """test 2 searching algo"""
-grid = generate_gridworld(201,201,.3)
+grid = generate_gridworld(101,101,.3)
 start = (0,0)
-end = (200,200)
+end = (100,100)
 agent1 = algorithmA(grid, start, end, has_four_way_vision = False)
 agent2 = algorithmA(grid, start, end, has_four_way_vision = True)
 agent3 = inference(grid, start, end)
+agent4 = inference(grid, start, end, is_expert = True)
+
+    
